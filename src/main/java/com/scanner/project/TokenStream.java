3bf40f60be64c0326c4b69a0f772a096e0f1d8ab
yineirya <yineiryyyyy12@gmail.com>
2025-12-11 20:10:29 -0500

    Refactor TokenStream for KAY implementation

--- src/main/java/com/scanner/project/TokenStream.java
+++ src/main/java/com/scanner/project/TokenStream.java
@@ -60,7 +60,7 @@ public class TokenStream {
 				while (!isEndOfLine(nextChar) && !isEof) {
 					nextChar = readChar();
 				}
-
+				skipWhiteSpace();
 			} else {
 				// A slash followed by anything else must be an operator.
 				t.setValue("/");
@@ -268,4 +268,5 @@ public class TokenStream {
 		return isEof;
 	}
 }
+
 
